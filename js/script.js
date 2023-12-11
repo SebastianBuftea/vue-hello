5a8b09314e181dfcdb6040f0233b09593c9c125d
SebastianBuftea <sebibuftea85@gmail.com>
2023-12-11 17:10:01 +0100

    [FINISH]

--- js/script.js
+++ js/script.js
@@ -2,7 +2,8 @@ const { createApp } = Vue
 createApp({
     data() {
         return{
-            message: "Hello Vue!"
+            message: "Hello Vue!",
+            photo:"https://www.geekandjob.com/uploads/wiki/f236a1aa005ef9a93041f294749e86d1.png"
         }
     }
 }).mount("#app")
